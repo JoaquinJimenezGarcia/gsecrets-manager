580cc969fa559e209fd49613fc7f8ec46fc98f5a
Joaquin Jimenez Garcia <jgarcia@inpost.pl>
2024-07-01 23:47:02 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,97 @@
+# Import the Secret Manager client library.
+from google.cloud import secretmanager
+
+def create_secret():
+    # GCP project in which to store secrets in Secret Manager.
+    project_id = "jjgdevelopment-399314"
+
+    # ID of the secret to create.
+    secret_id = "test-secret"
+
+    # Create the Secret Manager client.
+    client = secretmanager.SecretManagerServiceClient()
+
+    # Build the parent name from the project.
+    parent = f"projects/{project_id}"
+
+    # Create the parent secret.
+    secret = client.create_secret(
+        request={
+            "parent": parent,
+            "secret_id": secret_id,
+            "secret": {"replication": {"automatic": {}}},
+        }
+    )
+
+    # Add the secret version.
+    version = client.add_secret_version(
+        request={"parent": secret.name, "payload": {"data": b"hello world!"}}
+    )
+
+    print(version)
+
+    # Access the secret version.
+    response = client.access_secret_version(request={"name": version.name})
+
+    # Print the secret payload.
+    #
+    # WARNING: Do not print the secret in a production environment - this
+    # snippet is showing how to access the secret material.
+    payload = response.payload.data.decode("UTF-8")
+    print(f"Plaintext: {payload}")
+
+
+def access_secret_version(secret):
+    # Create a client
+    client = secretmanager.SecretManagerServiceClient()
+
+    # Initialize request argument(s)
+    request = secretmanager.AccessSecretVersionRequest(
+        name=secret,
+    )
+
+    # Make the request
+    response = client.access_secret_version(request=request)
+
+    # Handle the response
+    print(response.payload.data)
+
+def list_secret_versions():
+    # Create a client
+    client = secretmanager.SecretManagerServiceClient()
+
+    # Initialize request argument(s)
+    request = secretmanager.ListSecretVersionsRequest(
+        parent="projects/874147184475/secrets/test-secret",
+    )
+
+    # Make the request
+    page_result = client.list_secret_versions(request=request)
+    all_versions = []
+
+    # Handle the response
+    for response in page_result:
+        all_versions.append(response.name)
+    
+    return all_versions[0]
+
+def add_secret_version():
+    # Create a client
+    client = secretmanager.SecretManagerServiceClient()
+
+    # Initialize request argument(s)
+    request = secretmanager.AddSecretVersionRequest(
+        parent="parent_value",
+    )
+
+    # Make the request
+    response = client.add_secret_version(
+        request={"parent": "projects/874147184475/secrets/test-secret", "payload": {"data": b"created automatically"}}
+    )
+
+    # Handle the response
+    print(response)
+
+
+#access_secret_version(list_secret_versions())
+add_secret_version()
